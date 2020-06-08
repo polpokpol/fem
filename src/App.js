7a1755b973b4d329ebc1b146c5a81b3459b232fb
mc <mc@pop-os.localdomain>
2020-06-08 02:30:20 -0400

    starting to connect to the server

--- src/App.js
+++ src/App.js
@@ -57,6 +57,14 @@ class App extends Component{
     }
   }
 
+
+  // componentDidMount(){
+  //   fetch('http://localhost:3000/')
+  //     .then(response => response.json())
+  //     .then(console.log);
+  // }
+
+
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
